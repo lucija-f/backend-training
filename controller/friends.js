f53c6d011f6323dda7df3014550cc90497f75631
Lucija <52664508+lucija-f@users.noreply.github.com>
2022-02-08 17:09:55 +0100

    with validation

--- controller/friends.js
+++ controller/friends.js
@@ -1,4 +1,13 @@
+const Joi = require("joi");
 const { getAllFriends, postOneFriend } = require("../repo/friends");
+
+const schema = Joi.object({
+  first_name: Joi.string().alphanum().min(3).max(30).required(),
+
+  last_name: Joi.string().alphanum().min(3).max(30).required(),
+
+  nickname: Joi.string().allow(null).allow("").max(30),
+});
 
 async function getFriends(ctx) {
   ctx.body = await getAllFriends();
@@ -6,7 +15,10 @@ async function getFriends(ctx) {
 
 async function postFriend(ctx) {
   console.log(ctx.request.body);
-  ctx.body = await postOneFriend(ctx.request.body);
+  const result = schema.validate(ctx.request.body);
+  console.log(result);
+  if (result.error) ctx.body = "Not valid data";
+  else ctx.body = await postOneFriend(ctx.request.body);
 }
 
 module.exports = { getFriends, postFriend };
